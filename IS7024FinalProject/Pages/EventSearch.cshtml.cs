446f657ca5aedc3557a85d1b28321d6a14f4ccd8
hortonco <chorton5863@yahoo.com>
2025-11-03 20:39:37 -0500

    Started basic html on Event Search Page

---
+++ IS7024FinalProject/Pages/EventSearch.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IS7024FinalProject.Pages
+{
+    public class EventSearchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
